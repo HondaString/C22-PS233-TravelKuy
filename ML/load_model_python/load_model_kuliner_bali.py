f098e64778d41265d5c9540d39c0dc554a13a78c
aluthxander <aluthxander75@gmail.com>
2022-06-12 00:36:17 +0700

    menambahkan load model kuliner bali

---
+++ ML/load_model_python/load_model_kuliner_bali.py
@@ -0,0 +1,149 @@
+
+import numpy as np
+import pandas as pd 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+rating = pd.read_csv('rating_kuliner_bali.csv')
+kuliner = pd.read_csv('kuliner.csv')
+user = pd.read_csv('user.csv')
+
+kuliner = kuliner[kuliner['City']=='Bali']
+df = rating.copy()
+
+def dict_encoder(col, data=df):
+
+  # Mengubah kolom suatu dataframe menjadi list tanpa nilai yang sama
+  unique_val = data[col].unique().tolist()
+  # Melakukan encoding value kolom suatu dataframe ke angka
+  val_to_val_encoded = {x: i for i, x in enumerate(unique_val)}
+  # Melakukan proses encoding angka ke value dari kolom suatu dataframe
+  val_encoded_to_val = {i: x for i, x in enumerate(unique_val)}
+  return val_to_val_encoded, val_encoded_to_val
+
+user_to_user_encoded, user_encoded_to_user = dict_encoder('User_Id')
+
+# Mapping User_Id ke dataframe
+df['user'] = df['User_Id'].map(user_to_user_encoded)
+
+# Encoding Place_Id
+place_to_place_encoded, place_encoded_to_place = dict_encoder('Place_Id')
+
+# Mapping Place_Id ke dataframe place
+df['place'] = df['Place_Id'].map(place_to_place_encoded)
+
+# Mendapatkan jumlah user dan place
+num_users, num_place = len(user_to_user_encoded), len(place_to_place_encoded)
+ 
+# Mengubah rating menjadi nilai float
+df['Place_Ratings'] = df['Place_Ratings'].values.astype(np.float32)
+ 
+# Mendapatkan nilai minimum dan maksimum rating
+min_rating, max_rating = min(df['Place_Ratings']), max(df['Place_Ratings'])
+
+# Mengacak dataset
+df = df.sample(frac=1, random_state=42)
+
+# Membuat variabel x untuk mencocokkan data user dan place menjadi satu value
+x = df[['user', 'place']].values
+ 
+# Membuat variabel y untuk membuat rating dari hasil 
+y = df['Place_Ratings'].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
+ 
+# Membagi menjadi 80% data train dan 20% data validasi
+train_indices = int(0.8 * df.shape[0])
+x_train, x_val, y_train, y_val = (
+    x[:train_indices],
+    x[train_indices:],
+    y[:train_indices],
+    y[train_indices:]
+)
+
+class RecommenderNet(tf.keras.Model):
+ 
+  # Insialisasi fungsi
+  def __init__(self, num_users, num_places, embedding_size, **kwargs):
+    super(RecommenderNet, self).__init__(**kwargs)
+    self.num_users = num_users
+    self.num_places = num_places
+    self.embedding_size = embedding_size
+    self.user_embedding = layers.Embedding( # layer embedding user
+        num_users,
+        embedding_size,
+        embeddings_initializer = 'he_normal',
+        embeddings_regularizer = keras.regularizers.l2(1e-6)
+    )
+    self.user_bias = layers.Embedding(num_users, 1) # layer embedding user bias
+    self.places_embedding = layers.Embedding( # layer embeddings places
+        num_places,
+        embedding_size,
+        embeddings_initializer = 'he_normal',
+        embeddings_regularizer = keras.regularizers.l2(1e-6)
+    )
+    self.places_bias = layers.Embedding(num_places, 1) # layer embedding places bias
+ 
+  def call(self, inputs):
+    user_vector = self.user_embedding(inputs[:,0]) # memanggil layer embedding 1
+    user_bias = self.user_bias(inputs[:, 0]) # memanggil layer embedding 2
+    places_vector = self.places_embedding(inputs[:, 1]) # memanggil layer embedding 3
+    places_bias = self.places_bias(inputs[:, 1]) # memanggil layer embedding 4
+ 
+    dot_user_places = tf.tensordot(user_vector, places_vector, 2) 
+ 
+    x = dot_user_places + user_bias + places_bias
+    
+    return tf.nn.sigmoid(x) # activation sigmoid
+
+model_load = RecommenderNet(num_users, num_place, 50) # inisialisasi model
+ 
+# model compile
+model_load.compile(
+    loss = tf.keras.losses.BinaryCrossentropy(),
+    optimizer = keras.optimizers.Adam(learning_rate=0.0004),
+    metrics=[tf.keras.metrics.RootMeanSquaredError()]
+)
+
+model_load.load_weights('./model_kuliner_bali/my_checkpoint')
+
+
+# Menyiapkan dataframe
+place_df = kuliner[['Place_Id', 'Kuliner_Name', 'Description', 'Category', 'alamat', 'City',
+       'Rating', 'Coordinate', 'Lat', 'Long', 'Price', 'Images']]
+place_df.columns = ['Place_Id', 'Kuliner_Name', 'Description', 'Category', 'alamat', 'City',
+       'Rating', 'Coordinate', 'Lat', 'Long', 'Price', 'Images']
+df = rating.copy()
+
+# Mengambil sample user
+user_id = df.User_Id.sample(1).iloc[0]
+place_visited_by_user = df[df.User_Id == user_id]
+# Membuat data lokasi yang belum dikunjungi user
+place_not_visited = place_df[~place_df['Place_Id'].isin(place_visited_by_user.Place_Id.values)]['Place_Id'] 
+place_not_visited = list(
+    set(place_not_visited)
+    .intersection(set(place_to_place_encoded.keys()))
+)
+ 
+place_not_visited = [[place_to_place_encoded.get(x)] for x in place_not_visited]
+user_encoder = user_to_user_encoded.get(user_id)
+user_place_array = np.hstack(
+    ([[user_encoder]] * len(place_not_visited), place_not_visited)
+)
+# Mengambil top 5 recommendation
+ratings = model_load.predict(user_place_array).flatten()
+top_ratings_indices = ratings.argsort()[-5:][::-1]
+recommended_place_ids = [
+    place_encoded_to_place.get(place_not_visited[x][0]) for x in top_ratings_indices
+]
+ 
+
+print('')
+print('----' * 15)
+print('Top 5 place recommendation')
+print('----' * 15)
+ 
+recommended_place = place_df[place_df['Place_Id'].isin(recommended_place_ids)]
+for row, i in zip(recommended_place.itertuples(), range(1,8)):
+    print(i,'.', row.Kuliner_Name, '\n    ', row.alamat, ',', 'Rating Kuliner ', row.Rating,'\n')
+
+print('==='*15)
